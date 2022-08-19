effa64b538f096f1cb46f14c46ae64c2b946203a
EstherWanjiruUge <ugeesther@gmail.com>
2022-08-19 11:51:14 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const foundDrivers = drivers.filter((driver) => {
+    return driver.toLocaleLowerCase() === name.toLocaleLowerCase();
+  });
+  return foundDrivers;
+}
+
+function fuzzyMatch(drivers, name) {
+  const foundDrivers = drivers.filter((driver) => {
+    return driver.slice(0, 2).toLocaleLowerCase() === name.toLocaleLowerCase();
+  });
+  return foundDrivers;
+}
+
+function matchName(drivers, name) {
+  const foundDrivers = drivers.filter((driver) => {
+    return driver["name"].toLocaleLowerCase() === name.toLocaleLowerCase();
+  });
+  return foundDrivers;
+}
